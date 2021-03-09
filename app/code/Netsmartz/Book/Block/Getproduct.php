87e0b6c5694db99533070a836bd7302e2dd338dd
Debendra Prusty <debendra.prusty@netsmartz.net>
2021-03-09 22:27:06 +0530

    Elasticsearch version update

--- app/code/Netsmartz/Book/Block/Getproduct.php
+++ app/code/Netsmartz/Book/Block/Getproduct.php
@@ -14,7 +14,7 @@ class Getproduct extends \Magento\Framework\View\Element\Template
      */
     public function getProductlist()
     {
-        $apiURL= "localhost:9200/magento2_product_1_v4/_search?pretty&q=*:*";
+        $apiURL= "localhost:9200/magento2_product_1_v2/_search?pretty&q=*:*";
         $ch = curl_init($apiURL);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $result = curl_exec($ch);
